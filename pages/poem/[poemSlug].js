2f58726ff7d1fae0112773b9fefff71d3e811af8
Manish Yadav <60292480+Manishyadav514@users.noreply.github.com>
2023-01-14 18:29:52 +0530

    Update [poemSlug].js

--- pages/poem/[poemSlug].js
+++ pages/poem/[poemSlug].js
@@ -50,19 +50,33 @@ export default PoemSlug;
 //   };
 // }
 
-export async function getServerSideProps(context) {
-  const { poemSlug } = context.params;
-  console.log(poemSlug);
+//export async function getServerSideProps(context) {
+//  const { poemSlug } = context.params;
+ // console.log(poemSlug);
 
-  // let allb = await fs.promises.readFile(`data/poemData.json`)
-  // // let myBlog = await fs.promises.readFile(`data/blogData/${slug}.json`, 'utf-8')
-  // let myBlog = allb.filter(b => b.title === {slug})
+ // // let allb = await fs.promises.readFile(`data/poemData.json`)
+ // // // let myBlog = await fs.promises.readFile(`data/blogData/${slug}.json`, 'utf-8')
+//  // let myBlog = allb.filter(b => b.title === {slug})
 
-  let res = await fetch(`http://localhost:3000/api/poemAPI`);
-  let PoemData = await res.json();
-  let poem = PoemData.filter((b) => b.slug === poemSlug)[0];
-  console.log(poem);
-  return {
-    props: { poem }, // will be passed to the page component as props
+//  let res = await fetch(`http://localhost:3000/api/poemAPI`);
+ // let PoemData = await res.json();
+ // let poem = PoemData.filter((b) => b.slug === poemSlug)[0];
+ // console.log(poem);
+//  return {
+//    props: { poem }, // will be passed to the page component as props
+//  };
+//}
+
+
+
+export async function getStaticProps() {
+const { poemSlug } = context.params;
+let res = await fetch(`http://localhost:3000/api/poemAPI`);
+ let PoemData = await res.json();
+ let poem = PoemData.filter((b) => b.slug === poemSlug)[0];
+ console.log(poem);
+ return {
+   props: { poem }, // will be passed to the page component as props
   };
+  }
 }
