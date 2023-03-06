e798d0f5462b98ac8f2005e3619c08ad1e9fcc6e
Manish <manish.yadav@myglamm.com>
2023-03-06 12:12:26 +0530

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -54,86 +54,115 @@ export const Header1 = () => {
 
   return (
     <div
-      className={`z-[999] top-0 left-0 w-full flex justify-between items-center phone:px-[12px] py-[15px] px-[200px] tablet:p-[25px] phone:p-[15px] phone:bg-[#232a34] transition-all ${
+      className={`z-[999] top-0 left-0 w-full flex justify-between items-center px-[12px] py-[15px] md:px-[200px] md:p-[25px] p-[15px] transition-all ${
         homePath
-          ? "absolute phone:relative bg-transparent phone:bg-[#051A28] text-[#34173c]"
+          ? "md:absolute relative md:bg-transparent bg-[#051A28] text-[#34173c]"
           : "bg-[#051A28] text-white"
       }`}
       id="navbar"
     >
       <div className="flex align-middle items-center justify-center">
-        <Image
-          width={120}
-          height={50}
-          src={"logo/dearsenpai.webp"}
-          alt="logo"
-          priority
-        />
-        {/* <Link href="/">
-          <a className="text-base font-medium font-AmsterdamOne hover:text-pink-500">
-            Dear Senpai
-          </a>
-        </Link> */}
+        <Link href="/">
+          <Image
+            width={120}
+            height={50}
+            src={"logo/dearsenpai.webp"}
+            alt="logo"
+            priority
+            className="cursor-pointer"
+          />
+        </Link>
       </div>
       {/* <a className="relative text-white text-lg font-normal transition ease-in-out before:content[''] before:absolute before:bg-cyan-800 before:w-0 before:h-[3px] before:bottom-0 before:left-0 before:transition before:ease-in-out hover:text-cyan-400 hover:before:w-full ">
         manish
       </a> */}
       <div
-        className="cursor-pointer items-center hidden tablet:block phone:block"
+        className="cursor-pointer items-center lg:hidden md:block block"
         onClick={toggle}
       >
-        <FaBars className="text-white text-[26px]" />
+        <div
+          className="rounded p-1 md:p-2 cursor-pointer flex flex-row bg-[#222222] text-white items-center lg:hidden"
+          onClick={toggle}
+        >
+          <div className="mr-1 hidden md:block lg:inline-flex lg:w-auto w-full rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white">
+            Menu
+          </div>
+          <div className="text-[1.3rem]">
+            <FaBars />
+          </div>
+        </div>
       </div>
+      {/* top navigation bar */}
       <div
-        className={`text-[1em] font-medium ${
+        className={`hidden lg:block text-[1em] font-medium ${
           homePath ? "text-black" : "text-white"
         } ${styles.navbar_links}`}
       >
         <div className={` ${styles.navbar_link}`}>
-          <Link href="/">
-            <a className="hover:text-pink-500 pb-2 ml-[30px]">Home</a>
-          </Link>
-          <Link href="/poem">
-            <a className={`hover:text-pink-500 pb-2 ml-[30px]`}>Alfaaz-e-Sukhan</a>
-          </Link>
-          <Link href="/photography">
-            <a className="hover:text-pink-500 pb-2 ml-[30px]">Photography</a>
-          </Link>
-          <Link href="/about">
-            <a className="hover:text-pink-500 pb-2 ml-[30px]">About</a>
-          </Link>
+          {menuLink.map((item: any) => (
+            <Link key={item.label} href={item.slug}>
+              <a className="hover:text-pink-500 pb-2 ml-[30px]">
+                {item.label}
+                {/* {item?.child?.length > 0 && (
+                  <span className="text-xl">&#8250;</span>
+                )} */}
+              </a>
+            </Link>
+          ))}
         </div>
       </div>
       {/* <a className="relative hover:text-pink-400 cursor-pointer before:content[''] before:absolute before:bg-cyan-500 before:w-0 before-h-[3px] bottom-0 left-0 transition ease-in delay-[0.3s] hover:before:w-full">Contact</a> */}
+      {/* side navigation bar */}
       <div
-        className={isOpen ? styles.navbar_sidebar : styles.navbar_sidebar_open}
-      >
-        {/* <div
-        className={`fixed w-full h-full bg-[#2184a7] hidden tablet:grid phone:grid items-center z-[999] ${
-          isOpen ? "opacity-[1] transition-[0.5s] ease-in-out top-0 left-0 " : "opacity-0 transition-[1s] ease-in-out left-0 top-full"
+        className={`lg:hidden ${
+          isOpen
+            ? `fixed w-full h-full ease-linear transform transition duration-500 z-[999] left-0 top-0 bg-[#2184a7] opacity-100`
+            : `fixed w-full h-full ease-linear transform transition duration-500 z-[999] left-0 top-0 -translate-x-full opacity-0 bg-[#0d0d0d]`
         }`}
-      > */}
+      >
         <div
           className="absolute top-5 right-6 bg-transparent text-[2rem] cursor-pointer outline-none"
           onClick={toggle}
         >
-          <FaWindowClose color="#fff" />
+          <FaWindowClose />
         </div>
         <div
           className="h-full flex flex-col gap-5 mt-32 items-center text-[1.5rem] text-white"
           onClick={toggle}
         >
-          <Link href="/about">
-            <a className="">About</a>
-          </Link>
-          <Link href="/photography">
-            <a>Photo</a>
-          </Link>
-          <div>
-            <ButtonBW label="Sign In" darkButton={false} />
-          </div>
+          {menuLink.map((item: any) => (
+            <span
+              key={item.label}
+              className="p-2 mx-1 text-white hover:bg-[#E64238] rounded cursor-pointer"
+            >
+              <Link href={item.slug}>{item.label}</Link>
+            </span>
+          ))}
         </div>
       </div>
     </div>
   );
 };
+
+export const menuLink = [
+  {
+    label: "Home",
+    slug: "/",
+  },
+  {
+    label: "Blog",
+    slug: "/blog",
+  },
+  {
+    label: "Alfaaz-e-Sukhan",
+    slug: "/poem",
+  },
+  {
+    label: "Photography",
+    slug: "/photography",
+  },
+  {
+    label: "About",
+    slug: "/about",
+  },
+];
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import "../styles/globals.css";
-import { Header1 } from "../components/Header";
+import { Header1, Header2 } from "../components/Header";
 import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header1 />
+      {/* <Header2 /> */}
       <Component {...pageProps} />
       <Footer />
     </>
